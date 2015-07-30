9f27c8d484737dca890de51f1ac2a45047d7e980
Chris Hern <chrishern81@gmail.com>
2015-07-30 19:06:35 +0100

    Initial 2015 changes to only include the player data load and generate the FPL POJOs directly from the JSON schema.

--- fantasy-draft-fpl-integration/src/main/java/net/blackcat/fantasy/draft/fpl/integration/client/PlayerDataClient.java
+++ fantasy-draft-fpl-integration/src/main/java/net/blackcat/fantasy/draft/fpl/integration/client/PlayerDataClient.java
@@ -3,8 +3,8 @@
  */
 package net.blackcat.fantasy.draft.fpl.integration.client;
 
+import net.blackcat.fantasy.draft.fpl.integration.api.FantasyPremierLeaguePlayer;
 import net.blackcat.fantasy.draft.fpl.integration.exception.FantasyPremierLeagueException;
-import net.blackcat.fantasy.draft.fpl.integration.model.FantasyPremierLeaguePlayer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,22 +15,22 @@ import org.springframework.web.client.RestTemplate;
  * REST client which invokes the Fantasy Premier League REST endpoint to get player data.
  * 
  * @author Chris
- *
+ * 
  */
 @Service(value = "playerDataClient")
 public class PlayerDataClient {
 
-	@Autowired
-	@Qualifier(value = "fplRestTemplate")
-	private RestTemplate restTemplate;
-	
-	private static final String ENDPOINT = "http://fantasy.premierleague.com/web/api/elements/%s/";
+    @Autowired
+    @Qualifier(value = "fplRestTemplate")
+    private RestTemplate restTemplate;
 
-	public FantasyPremierLeaguePlayer getPlayer(int playerId) throws FantasyPremierLeagueException {
-		final String url = String.format(ENDPOINT, playerId);
-		
-		final FantasyPremierLeaguePlayer player = restTemplate.getForObject(url, FantasyPremierLeaguePlayer.class);
-		
-		return player;
-	}
+    private static final String ENDPOINT = "http://fantasy.premierleague.com/web/api/elements/%s/";
+
+    public FantasyPremierLeaguePlayer getPlayer(int playerId) throws FantasyPremierLeagueException {
+        final String url = String.format(ENDPOINT, playerId);
+
+        final FantasyPremierLeaguePlayer player = restTemplate.getForObject(url, FantasyPremierLeaguePlayer.class);
+
+        return player;
+    }
 }
--- fantasy-draft-fpl-integration/src/main/java/net/blackcat/fantasy/draft/fpl/integration/facade/PlayerDataFacade.java
+++ fantasy-draft-fpl-integration/src/main/java/net/blackcat/fantasy/draft/fpl/integration/facade/PlayerDataFacade.java
@@ -7,32 +7,14 @@ package net.blackcat.fantasy.draft.fpl.integration.facade;
  * Facade for Fantasy Premier League player data.
  * 
  * @author Chris
- *
+ * 
  */
 public interface PlayerDataFacade {
 
-	/**
-	 * Perform the initial population of player data from the Fantasy Premier League database to the Fantasy Draft database. 
-	 */
-	void performInitialPlayerDataLoad();
-	
-	/**
-	 * Populate the player scores for the current gameweek.
-	 */
-	void populatePlayerScores();
-	
-	/**
-	 * Populate the player scores for a historical gameweek.
-	 */
-	void populateHistoricalPlayerScores(int gameweek);
-	
-	/**
-	 * A one off method used to get the initial FPL purchase price of players that have been selected.
-	 */
-	void updatePlayersWithInitialPurchasePrice();
-	
-	/**
-	 * Update the current price of all players stored based on the current price within Fantasy Premier League.
-	 */
-	void updatePlayersCurrentPrice();
+    /**
+     * Perform the initial population of player data from the Fantasy Premier League database to the Fantasy Draft
+     * database.
+     */
+    void performInitialPlayerDataLoad();
+
 }
